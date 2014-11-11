28d9744bae32668849136c158a05c3ad3f9ae078
Chris Hokamp <chris.hokamp@gmail.com>
2014-11-11 00:32:09 +0000

    mock EbmtParser

---
+++ parsers/EbmtParser.js
@@ -0,0 +1,29 @@
+// for now, pass in the file names and lang names
+var fs = require('fs')
+var assert = require('assert')
+
+function zip(arrays) {
+  return arrays[0].map(function(_,i){
+    return arrays.map(function(array){return array[i]})
+  });
+}
+
+var createTMObj = function(str, lang) {
+  return {lang: lang, segment: str};
+}
+
+module.exports.parseFiles = function(tmFile1, tmFile2) {
+  var lines1 = fs.readFileSync(tmFile1.filename, {encoding: 'utf8'}).split('\n');
+  var lines2 = fs.readFileSync(tmFile2.filename, {encoding: 'utf8'}).split('\n');
+  assert.equal(lines1.length, lines2.length);
+
+  var segs1 = lines1.map(function(seg) {
+    return createTMObj(seg, tmFile1.lang)
+  });
+  var segs2 = lines2.map(function(seg) {
+    return createTMObj(seg, tmFile2.lang)
+  });
+
+  return zip([segs1,segs2]);
+}
+
---
+++ test/EbmtParser.spec.js
@@ -0,0 +1,31 @@
+var EbmtParser = require('../parsers/EbmtParser');
+
+var tmFile1;
+var tmFile2;
+
+
+beforeEach(
+  function() {
+    // remember that paths are relative to the EbmtParser file location
+    var filename1 = 'test/1000.en';
+    var filename2 = 'test/1000.de';
+    var lang1 = 'en';
+    var lang2 = 'de';
+    tmFile1 = {lang: lang1, filename: filename1};
+    tmFile2 = {lang: lang2, filename: filename2};
+  }
+);
+
+describe('EbmtParser tests', function () {
+
+  it('should open two files and return a list of segment objects', function() {
+    var segs = EbmtParser.parseFiles(tmFile1, tmFile2);
+    console.log('segs:');
+    console.log(segs.slice(0,10));
+    expect(segs[100].length).toEqual(2);
+    expect(segs[100][0].lang).toEqual('en');
+    expect(segs[100][1].lang).toEqual('de');
+  });
+
+});
+
